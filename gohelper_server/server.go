e94258bf0f52dc43cd65079cf3f2de3e3f4ee457
韩西瓜 <sohanyu@126.com>
2023-04-12 12:34:32 +0800

    feat: 修改server构造方式为build模式

--- gohelper_server/server.go
+++ gohelper_server/server.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var engine *gin.Engine
+//var GlobalEngine *gin.Engine
 
 type Router struct {
 	Prefix   string
@@ -13,47 +13,56 @@ type Router struct {
 	Handlers []gin.HandlerFunc
 }
 
-type Routers struct {
+type Server struct {
 	Get     []*Router
 	Post    []*Router
 	Delete  []*Router
 	Put     []*Router
 	Patch   []*Router
 	Options []*Router
+	Engine  *gin.Engine
 }
 
-func GetRoutersInstance() *Routers {
-	return &Routers{}
+// GetServerInstance 构造Server实例
+func GetServerInstance() *Server {
+	return &Server{Engine: gin.Default()}
 }
 
-func (routers *Routers) BuildGet(getRouters []*Router) *Routers {
-	routers.Get = getRouters
-	return routers
+func (server *Server) BuildGet(getRouters []*Router) *Server {
+	server.Get = getRouters
+	return server
 }
 
-func (routers *Routers) BuildPost(getRouters []*Router) *Routers {
-	routers.Post = getRouters
-	return routers
+func (server *Server) BuildPost(getRouters []*Router) *Server {
+	server.Post = getRouters
+	return server
 }
 
-func (routers *Routers) BuildDelete(getRouters []*Router) *Routers {
-	routers.Delete = getRouters
-	return routers
+func (server *Server) BuildDelete(getRouters []*Router) *Server {
+	server.Delete = getRouters
+	return server
 }
 
-func (routers *Routers) BuildPut(getRouters []*Router) *Routers {
-	routers.Put = getRouters
-	return routers
+func (server *Server) BuildPut(getRouters []*Router) *Server {
+	server.Put = getRouters
+	return server
 }
 
-func (routers *Routers) BuildPatch(getRouters []*Router) *Routers {
-	routers.Patch = getRouters
-	return routers
+func (server *Server) BuildPatch(getRouters []*Router) *Server {
+	server.Patch = getRouters
+	return server
 }
 
-func (routers *Routers) BuildOption(getRouters []*Router) *Routers {
-	routers.Options = getRouters
-	return routers
+func (server *Server) BuildOption(getRouters []*Router) *Server {
+	server.Options = getRouters
+	return server
+}
+
+func (server *Server) BuildUsesFunc(uses ...gin.HandlerFunc) *Server {
+	if uses != nil && len(uses) != 0 {
+		server.Engine.Use(uses...)
+	}
+	return server
 }
 
 // NewRouter 实例化一个新的路由
@@ -63,48 +72,45 @@ func NewRouter(prefix string, Uri string, Handlers []gin.HandlerFunc) *Router {
 	}
 }
 
-// StartServer 启动服务
-func StartServer(routers *Routers, ipPort string, uses ...gin.HandlerFunc) *gin.Engine {
+// Build 启动服务
+func (server *Server) Build(ipPort string) *gin.Engine {
 	//engine = gin.New()
 	//engine.Use(gin.Recovery())
-	engine = gin.Default()
+	//engine = gin.Default()
 
-	if uses != nil && len(uses) != 0 {
-		engine.Use(uses...)
+	for _, router := range server.Get {
+		server.Engine.GET(router.Uri, router.Handlers...)
 	}
 
-	for _, router := range routers.Get {
-		engine.GET(router.Uri, router.Handlers...)
+	for _, router := range server.Delete {
+		server.Engine.DELETE(router.Uri, router.Handlers...)
 	}
 
-	for _, router := range routers.Delete {
-		engine.DELETE(router.Uri, router.Handlers...)
+	for _, router := range server.Put {
+		server.Engine.PUT(router.Uri, router.Handlers...)
 	}
 
-	for _, router := range routers.Put {
-		engine.PUT(router.Uri, router.Handlers...)
+	for _, router := range server.Post {
+		server.Engine.POST(router.Uri, router.Handlers...)
 	}
 
-	for _, router := range routers.Post {
-		engine.POST(router.Uri, router.Handlers...)
+	for _, router := range server.Patch {
+		server.Engine.PATCH(router.Uri, router.Handlers...)
 	}
 
-	for _, router := range routers.Patch {
-		engine.PATCH(router.Uri, router.Handlers...)
-	}
-
-	for _, router := range routers.Options {
-		engine.OPTIONS(router.Uri, router.Handlers...)
+	for _, router := range server.Options {
+		server.Engine.OPTIONS(router.Uri, router.Handlers...)
 	}
 
 	if ipPort == "" {
 		ipPort = ":8080"
 	}
-	err := engine.Run(ipPort)
+
+	err := server.Engine.Run(ipPort)
 
 	if err != nil {
 		fmt.Printf("Gin engine start error is :%v", err)
 		return nil
 	}
-	return engine
+	return server.Engine
 }
